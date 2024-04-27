0e45b16aa4cfef9348dea627e68dfc8cca08dc1f
Jocygrajeda <122635036+Jocygrajeda@users.noreply.github.com>
2024-04-27 00:44:23 -0500

    STEP 5, there's an error but idk what it is

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => { res.send('Hello W');});
+//router.get('/', (req, res) => { res.send('Hello W');});
+
+router.use('/users', require('/users'));
 
 module.exports = router;
